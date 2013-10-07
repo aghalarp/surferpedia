92a9b11e76c65eca561e32aabdc6bd840862a194
David Aghalarpour <aghalarp@hawaii.edu>
2013-10-06 16:18:15 -1000

    Initial commit of source code.

---
+++ app/controllers/Application.java
@@ -0,0 +1,59 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.Index;
+import views.html.Page1;
+import views.html.Souza;
+import views.html.Brooke;
+import views.html.Ivarra;
+
+/**
+ * Implements the controllers for this application.
+ */
+public class Application extends Controller {
+
+  /**
+   * Returns the home page. 
+   * @return The resulting home page. 
+   */
+  public static Result index() {
+    return ok(Index.render("Welcome to the home page."));
+  }
+  
+  /**
+   * Returns page1, a simple example of a second page to illustrate navigation.
+   * @return The Page1.
+   */
+  public static Result page1() {
+    return ok(Page1.render("Welcome to Page1."));
+    
+  }
+  
+  /**
+   * Returns Souza's bio page.
+   * @return The Souza page
+   */
+  public static Result souza() {
+    return ok(Souza.render("Welcome to Page1."));
+    
+  }
+  
+  /**
+   * Returns Brooke's bio page.
+   * @return The Brooke page.
+   */
+  public static Result brooke() {
+    return ok(Brooke.render("Welcome to Page1."));
+    
+  }
+  
+  /**
+   * Returns Ivarra's bio page.
+   * @return The Ivarra page.
+   */
+  public static Result ivarra() {
+    return ok(Ivarra.render("Welcome to Page1."));
+    
+  }
+}
