66a9282949378aa9a2db729506379bbc8a66888b
Kevin Paek <kpaekn@gmail.com>
2013-12-02 15:19:53 -1000

    removed the dropdown of surfers in the navbar.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,55 +16,61 @@ import views.html.Login;
 import views.html.ManageSurfer;
 import views.html.ShowSurfer;
 
-
 /**
  * Implements the controllers for this application.
  */
 public class Application extends Controller {
 
   /**
-   * Returns the home page. 
-   * @return The resulting home page. 
+   * Returns the home page.
+   * 
+   * @return The resulting home page.
    */
   public static Result index() {
     return ok(Index.render("Home", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), SurferDB.getSurfers()));
   }
-  
+
   /**
    * Returns the ManageSurfer page with empty form fields. For creating new Surfers.
+   * 
    * @return The ManageSurfer page.
    */
   @Security.Authenticated(Secured.class)
   public static Result newSurfer() {
-    SurferFormData data = new SurferFormData(); //Create empty surferFormData object
-    Form<SurferFormData> formData = Form.form(SurferFormData.class).fill(data); //Then fill it with form data.
-    Map<String, Boolean> surferTypeMap = SurferTypes.getTypes(data.surferType); //Note: I think we can use empty param.
+    SurferFormData data = new SurferFormData(); // Create empty surferFormData object
+    Form<SurferFormData> formData = Form.form(SurferFormData.class).fill(data); // Then fill it with form data.
+    Map<String, Boolean> surferTypeMap = SurferTypes.getTypes(data.surferType); // Note: I think we can use empty param.
     List<String> footstyleTypeList = FootstyleTypes.getTypes();
-    return ok(ManageSurfer.render("NewSurfer", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), formData, surferTypeMap, footstyleTypeList, SurferDB.getSurfers()));
+    return ok(ManageSurfer.render("NewSurfer", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), formData,
+        surferTypeMap, footstyleTypeList));
   }
-  
+
   /**
    * Returns the ManageSurfer page with pre-filled form fields. For editing existing Surfers.
+   * 
    * @param slug The slug.
    * @return The ManageSurfer page.
    */
   @Security.Authenticated(Secured.class)
   public static Result manageSurfer(String slug) {
-    SurferFormData data = new SurferFormData(SurferDB.getSurfer(slug)); //Create new but existing surferFormData object
+    SurferFormData data = new SurferFormData(SurferDB.getSurfer(slug)); // Create new but existing surferFormData object
     Form<SurferFormData> formData = Form.form(SurferFormData.class).fill(data);
     Map<String, Boolean> surferTypeMap = SurferTypes.getTypes(data.surferType);
     List<String> footstyleTypeList = FootstyleTypes.getTypes();
-    return ok(ManageSurfer.render("EditSurfer", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), formData, surferTypeMap, footstyleTypeList, SurferDB.getSurfers()));
+    return ok(ManageSurfer.render("EditSurfer", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), formData,
+        surferTypeMap, footstyleTypeList));
   }
-  
+
   /**
    * Handles the posting of form data by the user.
+   * 
    * @return The ManageSurfer page with form data.
    */
   public static Result postSurfer() {
     Form<SurferFormData> formData = Form.form(SurferFormData.class).bindFromRequest();
-    
-    /* Important to understand: Whenever we invoke bindFromRequest(), if there is a validation() method in the
+
+    /*
+     * Important to understand: Whenever we invoke bindFromRequest(), if there is a validation() method in the
      * associated object's class (in this case, SurferFormData) that implements the form, it will call that validation
      * method and annotate that object with information about any found errors (We see that the validate() method
      * returns either null or a list of ValidationErrors). We can then check if the formData object contains any errors
@@ -74,63 +80,67 @@ public class Application extends Controller {
      * else clause.
      */
     if (formData.hasErrors()) {
-      //Create empty surferTypeMap
+      // Create empty surferTypeMap
       Map<String, Boolean> surferTypeMap = SurferTypes.getTypes();
       List<String> footstyleTypeList = FootstyleTypes.getTypes();
-      return badRequest(ManageSurfer.render("ManageSurfer", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), formData, surferTypeMap, footstyleTypeList, SurferDB.getSurfers()));
+      return badRequest(ManageSurfer.render("ManageSurfer", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()),
+          formData, surferTypeMap, footstyleTypeList));
     }
     else {
-      SurferFormData data = formData.get(); //Creates the object we made (SurferFormData) and fills with get data
-      
-      //Add to database
+      SurferFormData data = formData.get(); // Creates the object we made (SurferFormData) and fills with get data
+
+      // Add to database
       SurferDB.addSurfer(data);
-      
+
       Map<String, Boolean> surferTypeMap = SurferTypes.getTypes(data.surferType);
       List<String> footstyleTypeList = FootstyleTypes.getTypes();
-      return ok(ManageSurfer.render("ManageSurfer", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), formData, surferTypeMap, footstyleTypeList, SurferDB.getSurfers()));
+      return ok(ManageSurfer.render("ManageSurfer", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), formData,
+          surferTypeMap, footstyleTypeList));
     }
-    
+
   }
-  
+
   /**
    * Returns the surfer bio page.
+   * 
    * @param slug The slug of surfer..
    * @return The requested surfer bio page.
    */
   public static Result getSurfer(String slug) {
-    return ok(ShowSurfer.render("ShowSurfer", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), SurferDB.getSurfer(slug), SurferDB.getSurfers()));
+    return ok(ShowSurfer.render("ShowSurfer", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()),
+        SurferDB.getSurfer(slug)));
   }
-  
+
   /**
    * Deletes requested Surfer from database.
+   * 
    * @param slug The slug of the Surfer to delete.
    * @return The index page.
    */
   @Security.Authenticated(Secured.class)
   public static Result deleteSurfer(String slug) {
     SurferDB.deleteSurfer(slug);
-    //Note, UpdateDB delete event is done in SurferDB.deleteSurfer() method. Could also move that code here...
-    
+    // Note, UpdateDB delete event is done in SurferDB.deleteSurfer() method. Could also move that code here...
+
     return ok(Index.render("Home", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), SurferDB.getSurfers()));
-  }
-  
-  
-  /**
-   * Provides the Login page (only to unauthenticated users). 
-   * @return The Login page. 
-   */
-  public static Result login() {
-    Form<LoginFormData> formData = Form.form(LoginFormData.class);
-    return ok(Login.render("Login", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), formData, SurferDB.getSurfers()));
   }
 
   /**
-   * Processes a login form submission from an unauthenticated user. 
-   * First we bind the HTTP POST data to an instance of LoginFormData.
-   * The binding process will invoke the LoginFormData.validate() method.
-   * If errors are found, re-render the page, displaying the error data. 
-   * If errors not found, render the page with the good data. 
-   * @return The index page with the results of validation. 
+   * Provides the Login page (only to unauthenticated users).
+   * 
+   * @return The Login page.
+   */
+  public static Result login() {
+    Form<LoginFormData> formData = Form.form(LoginFormData.class);
+    return ok(Login.render("Login", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), formData));
+  }
+
+  /**
+   * Processes a login form submission from an unauthenticated user. First we bind the HTTP POST data to an instance of
+   * LoginFormData. The binding process will invoke the LoginFormData.validate() method. If errors are found, re-render
+   * the page, displaying the error data. If errors not found, render the page with the good data.
+   * 
+   * @return The index page with the results of validation.
    */
   public static Result postLogin() {
 
@@ -139,7 +149,7 @@ public class Application extends Controller {
 
     if (formData.hasErrors()) {
       flash("error", "Login credentials not valid.");
-      return badRequest(Login.render("Login", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), formData, SurferDB.getSurfers()));
+      return badRequest(Login.render("Login", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), formData));
     }
     else {
       // email/password OK, so now we set the session variable and only go to authenticated pages.
@@ -148,10 +158,11 @@ public class Application extends Controller {
       return redirect(routes.Application.index());
     }
   }
-  
+
   /**
-   * Logs out (only for authenticated users) and returns them to the Index page. 
-   * @return A redirect to the Index page. 
+   * Logs out (only for authenticated users) and returns them to the Index page.
+   * 
+   * @return A redirect to the Index page.
    */
   @Security.Authenticated(Secured.class)
   public static Result logout() {
